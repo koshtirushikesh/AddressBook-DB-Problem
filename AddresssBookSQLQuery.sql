f44c5d3cea9fe565b56d03a7342fa42f5e417cdb
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-01-14 19:36:57 +0530

    Creating Address Book Table

--- AddresssBookSQLQuery.sql
+++ AddresssBookSQLQuery.sql
@@ -1,2 +1,15 @@
 create database AddressBook
 
+create table AddressBookTable
+( 
+FirstName varchar(225),
+LastName varchar(225),
+Address varchar(225),
+City varchar(225),
+State varchar(225),
+Zip varchar(8),
+PhoneNumber varchar(10),
+Email varchar(225),
+)
+
+select * from addressbooktable
