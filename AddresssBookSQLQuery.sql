a014c6666705946897ae16d12d2dc344132b29f0
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-01-14 19:53:30 +0530

    insert new Contacts to Address Book

--- AddresssBookSQLQuery.sql
+++ AddresssBookSQLQuery.sql
@@ -12,4 +12,9 @@ PhoneNumber varchar(10),
 Email varchar(225),
 )
 
+insert into AddressBookTable values('Bruce','Wane','12 Street','New York','New York','400078','8898338339','brucewane3@gmail.com')
+insert into AddressBookTable values('Ron','Wane','12 Street','Manhattan','New York','400078','8897888339','ronwane3@gmail.com')
+insert into AddressBookTable values('Jon','Henson','12 Street','Manhattan','New York','400078','8898334561','jonwane3@gmail.com')
+insert into AddressBookTable values('Rushi','Koshti','12 Street','Mumbai','Maharastra','400078','7894561223','rushi3@gmail.com')
+
 select * from addressbooktable
